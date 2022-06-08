1640456d97a2f7c231857e35f7fc873036423aca
Quang Minh <nguyendoquangminh2112@gmail.com>
2022-06-08 16:35:06 +0700

    upload code new

--- src/components/Layout/Tour/InfoTour.js
+++ src/components/Layout/Tour/InfoTour.js
@@ -292,7 +292,7 @@ const InfoTour = () => {
       <div
         class="fb-comments"
         data-href="https://developers.facebook.com/docs/plugins/comments#configurator"
-        data-width=""
+        data-width="100%"
         data-numposts="5"
       ></div>
     </Container>
